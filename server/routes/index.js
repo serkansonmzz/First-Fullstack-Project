83eb2882dcdce097dff5dfda9b990da13a622704
Serkan Sönmez <serkansonmzz@gmail.com>
2024-01-17 00:02:19 +0300

    arrangements and updates were made

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import userRouter from "./usersRouter.js";
-import postRouter from "./postRouter.js";
-const router = Router();
-
-router.use(userRouter);
-router.use(postRouter);
-
-export default router;
