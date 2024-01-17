02afca819538078219b7d703e2c2dccc72ffcafa
Serkan Sönmez <serkansonmzz@gmail.com>
2024-01-18 01:04:34 +0300

    models operations were integrated into contoller structures

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -1,11 +1,55 @@
-import posts from "../data/post.js";
+import postModel from "./../models/postModel.js";
 
 const postController = {
   getAllPosts: (req, res) => {
-    if (!posts) {
-      res.status(404).send("No posts found");
+    postModel
+      .getAllPosts()
+      .then((posts) => res.status(200).json(posts))
+      .catch((error) => res.status(500).json(error));
+  },
+  getPostById: (req, res) => {
+    const id = req.params.id;
+    if (isNaN(Number(id))) {
+      res.status(400).send("Id must be a number");
     }
-    res.status(200).json(posts);
+    postModel
+      .getPost(id)
+      .then((post) => res.status(200).json(post))
+      .catch((error) => res.status(500).json(error));
+  },
+  createPost: (req, res) => {
+    postModel
+      .addPost(req.body)
+      .then(() => res.status(201).json({ message: "Post created" }))
+      .catch((error) => res.status(500).json(error));
+  },
+  updatePost: (req, res) => {
+    const {
+      params: { id },
+      body,
+    } = req;
+
+    if (isNaN(Number(id))) {
+      res.status(400).send("Id must be a number");
+    }
+    if (!body.title || !body.content) {
+      res.status(400).send("Title and Content are required");
+    }
+
+    postModel
+      .updatePost(body, id)
+      .then(() => res.status(200).json({ message: "Post updated" }))
+      .catch((error) => res.status(500).json(error));
+  },
+  deletePost: (req, res) => {
+    const id = req.params.id;
+    if (isNaN(Number(id))) {
+      res.status(400).send("Id must be a number");
+    }
+    postModel
+      .deletePost(id)
+      .then(() => res.status(200).json({ message: "Post deleted" }))
+      .catch((error) => res.status(500).json(error));
   },
 };
 
--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,26 +1,72 @@
-import users from "./../data/user.js";
+import userModel from "./../models/userModel.js";
 
 const userController = {
   getAllUsers: (req, res) => {
-    if (!users) {
-      res.status(404).send("No users found");
-    }
-    res.status(200).send(users);
+    userModel
+      .getUsers()
+      .then((users) => res.status(200).send(users))
+      .catch((error) =>
+        res.status(500).send("Internal server error ", error.message)
+      );
   },
-  getUser: (req, res) => {
+  getUserById: (req, res) => {
     const id = req.params.id;
-    try {
-      if (isNaN(Number(id))) {
-        res.status(400).send("Id must be a number");
-      }
-      const user = users.find((user) => user.id === parseInt(id));
-      if (!user) {
-        res.status(404).send("No user found");
-      }
-      res.send(user);
-    } catch (error) {
-      return res.status(500).send("Internal server error ", error.message);
+    if (isNaN(Number(id))) {
+      res.status(400).send("Id must be a number");
     }
+    userModel
+      .getUser(id)
+      .then((user) => {
+        if (user) {
+          res.send(user);
+        } else {
+          res.status(404).send("User not found");
+        }
+      })
+      .catch((error) =>
+        res.status(500).send("Internal server error ", error.message)
+      );
+  },
+  createUser: (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.email) {
+      res.status(400).send("Name and email are required");
+    }
+    userModel
+      .addUser(user)
+      .then(() => res.status(201).send("User created"))
+      .catch((error) =>
+        res.status(500).send("Internal server error ", error.message)
+      );
+  },
+  updateUser: (req, res) => {
+    const id = req.params.id;
+    const user = req.body;
+
+    if (isNaN(Number(id))) {
+      res.status(400).send("Id must be a number");
+    }
+    if (!user.name || !user.email) {
+      res.status(400).send("Name and email are required");
+    }
+    userModel
+      .updateUser(user, id)
+      .then(() => res.status(200).send("User updated"))
+      .catch((error) =>
+        res.status(500).send("Internal server error ", error.message)
+      );
+  },
+  deleteUser: (req, res) => {
+    const id = req.params.id;
+    if (isNaN(Number(id))) {
+      res.status(400).send("Id must be a number");
+    }
+    userModel
+      .deleteUser(id)
+      .then(() => res.status(200).send("User deleted"))
+      .catch((error) =>
+        res.status(500).send("Internal server error ", error.message)
+      );
   },
 };
 
