2f63ecadd31d5253b6a16315f0975961ba076419
Serkan Sönmez <serkansonmzz@gmail.com>
2024-01-17 16:59:14 +0300

    server.js and index.js structures have been updated

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,9 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
-
 const app = express();
 app.use(cors());
 
 app.use(routes);
 
-const PORT = 8080;
-
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+export default app;
