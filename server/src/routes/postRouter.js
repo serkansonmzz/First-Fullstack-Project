4199af7c05064099aadaaab342ccbadeebc8bcc5
Serkan Sönmez <serkansonmzz@gmail.com>
2024-01-18 01:06:14 +0300

    controller operations were integrated into the router structure

--- server/src/routes/postRouter.js
+++ server/src/routes/postRouter.js
@@ -3,6 +3,10 @@ import postController from "../controllers/postController.js";
 
 const router = Router();
 
-router.get("/posts",postController.getAllPosts);
+router.get("/posts", postController.getAllPosts);
+router.get("/posts/:id", postController.getPostById);
+router.post("/posts", postController.createPost);
+router.put("/posts/:id", postController.updatePost);
+router.delete("/posts/:id", postController.deletePost);
 
 export default router;
--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -3,7 +3,9 @@ import userController from "./../controllers/userController.js";
 const router = Router();
 
 router.get("/users", userController.getAllUsers);
-
-router.get("/user/:id", userController.getUser);
+router.get("/users/:id", userController.getUserById);
+router.post("/users", userController.createUser);
+router.put("/users/:id", userController.updateUser);
+router.delete("/users/:id", userController.deleteUser);
 
 export default router;
